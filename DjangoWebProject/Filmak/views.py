36afe8627f9d53596de7a163e0bdf11d22ab12dd
IÑIGO AYESTARAN LÁZARO <iayestaran009@ikasle.ehu.eus>
2023-06-01 05:15:22 +0200

    vOTAR FUNCIONA

--- DjangoWebProject/Filmak/views.py
+++ DjangoWebProject/Filmak/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.core.paginator import Paginator
 
 from .forms import LoginForm, RegistroForm
-from .models import AuthUser, Filma
+from .models import AuthUser, Filma, GogokoFilmak, FilmaBozkatzailea, Erabiltzailea
 
 def home(request):
     request.session.setdefault('columna_izquierda_template', 'columna_izquierda.html')
@@ -33,7 +33,7 @@ def login(request):
             try:
                 user = AuthUser.objects.get(username=username)
                 if user.password == password:
-                    auth_login(request, user)
+                    request.session['username'] = username
                     request.session['columna_izquierda_template'] = 'columna_izquierda_logged.html'
                     return render(request, 'base.html')
                 else:
@@ -51,6 +51,7 @@ def logout(request):
     request.session.setdefault('columna_izquierda_template', 'columna_izquierda.html')
     columna_izquierda_template = request.session['columna_izquierda_template']
     request.session['columna_izquierda_template'] = 'columna_izquierda.html'
+    request.session['username'] = ''
     return render(request, 'base.html', {'columna_izquierda_template': columna_izquierda_template})
 
 def ver_pelis(request):
@@ -61,10 +62,41 @@ def ver_pelis(request):
     return render(request, 'ver_pelis.html', {'filmak': filmak})
 
 def votar(request):
+    filmak = Filma.objects.all()
     if request.method == 'POST':
-        # Lógica para procesar el formulario de votación
-        return redirect('home')
-    return render(request, 'votar.html')
+        mensaje = "¡Voto registrado con éxito!"
+        pelicula_id = request.POST.get('pelicula')
+        username = request.session['username']
+        user = AuthUser.objects.get(username=username)
+
+        try:
+            erabiltzailea = Erabiltzailea.objects.get(auth_user=user)
+        except Erabiltzailea.DoesNotExist:
+            # Si Erabiltzailea no existe, crearlo junto con FilmaBozkatzailea
+            filma_bozkatzailea = FilmaBozkatzailea.objects.create(erabiltzailea_id=user.id)
+            erabiltzailea = Erabiltzailea.objects.create(auth_user=user, filma_bozkatzailea=filma_bozkatzailea)
+
+        # Verificar si el usuario ya ha votado la película
+        if not GogokoFilmak.objects.filter(filma_id=pelicula_id, filma_gogokoa=erabiltzailea.filma_bozkatzailea).exists():
+            filma = Filma.objects.get(id=pelicula_id)
+
+            # Crear instancia de GogokoFilmak
+            gogoko_filmak = GogokoFilmak.objects.create(filma=filma, filma_gogokoa=erabiltzailea.filma_bozkatzailea)
+
+            # Incrementar el campo 'bozkak' de la película en uno
+            filma.bozkak += 1
+
+            # Guardar los cambios en los modelos
+            gogoko_filmak.save()
+            filma.save()
+
+        else:
+            mensaje = "Ya has votado esta película previamente."
+
+        return render(request, 'votar.html', {'mensaje': mensaje, 'filmak': filmak})
+
+    return render(request, 'votar.html', {'filmak': filmak})
+
 
 def seguidores(request):
     # Lógica para obtener la información de los seguidores y las películas votadas
